6756149da129e99a66781046f4d9eca01a66414b
Aaron Obed Giron <aogarrido.g@gmail.com>
2024-06-02 01:56:05 -0600

    Cambio: mandar las credenciales a la API con BasicAuth

--- Program.cs
+++ Program.cs
@@ -2,9 +2,19 @@ using Microsoft.AspNetCore.Http.Json;
 using Microsoft.Extensions.Options;
 using Microsoft.Net.Http.Headers;
 using System.Net.Http.Headers;
+using System.Text;
 using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
+// Configuración del cliente HTTP con credenciales de autenticación básica
+IConfiguration config = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.Development.json", optional: true, reloadOnChange: true)
+            .Build();
+
+string usuario = config.GetValue<string>("credenciales:usuario");
+string contrasena = config.GetValue<string>("credenciales:contrasena");
+
+var credentials = Convert.ToBase64String(Encoding.UTF8.GetBytes($"{usuario}:{contrasena}"));
 
 // Add services to the container.
 builder.Services.AddRazorPages();
@@ -16,6 +26,7 @@ builder.Services.AddHttpClient("FruitAPI", httpClient =>
 {
     //Establecer la dirreccion base para las solicitudes HTTP
     httpClient.BaseAddress = new Uri("http://localhost:5050/fruitlist/");
+    httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", credentials);
 });
 
 // End of HTTP client code
